72af61f32c9db0c70a7da3fdeedcc7dabf915e47
josemart <joselitan@gmail.com>
2019-06-10 21:33:49 +0200

    Initial commit

---
+++ exercises/exercise_1_name_age.py
@@ -0,0 +1,13 @@
+a = input('Enter your name: ')
+b = int(input('Enter your age: '))
+c = 2019 + (100 - b)# 64 year
+antal = int(input('Print out copies: '))
+statement = "Your name is: " + a + " and in: " + str(c) + " you will be 100 years old"
+print((statement + "\n") * antal)
+# print(('In year:', c, a + ' will be 100 years') * antal)
+# i = 0
+# while (i < antal):
+#     print('In year:', c, a + ' will be 100 years')
+#     i = i + 1
+
+
